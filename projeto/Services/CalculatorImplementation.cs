4c556ebc9ba34450df7b36fac3b7bba6cbd8e8b6
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-27 19:22:46 -0300

    ajeitando a organização das pastas

---
+++ projeto/Services/CalculatorImplementation.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto.Services
+{
+    public class CalculatorImplementation
+    {
+        public double Somar (double n , double n2)
+        {
+            return n + n2;
+        }
+          public double multiplicar (double n , double n2)
+        {
+            return n * n2;
+        }
+
+          public double subtrair (double n , double n2)
+        {
+            return n - n2;
+        }
+
+          public double dividir (double n , double n2)
+        {
+            return n / n2;
+        }
+    }
+}
