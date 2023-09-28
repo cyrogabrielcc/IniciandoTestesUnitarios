7119c72e150ad8839cc64063265f6b78b62ae316
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-28 20:43:54 -0300

    criando os testes

--- projetoTeste/ProjTest.cs
+++ projetoTeste/ProjTest.cs
@@ -1,10 +1,26 @@
+using projeto.Services;
+
 namespace projetoTeste;
 
-public class UnitTest1
+public class ProjTest
 {
-    [Fact]
-    public void Test1()
+    public CalculatorImplementation _calc;
+
+    public ProjTest()
     {
-        
+        _calc = new CalculatorImplementation();
+    }
+
+    [Fact]
+    public void ExecutaSoma()
+    {
+        var var1 = 5;
+        var var2 = 5;
+        var resultSoma = _calc.Somar(var1, var2);
+        Assert.Equal(10, resultSoma);
+
+
+        var result = _calc.subtrair(var1, var2);
+        Assert.Equal(0, result);
     }
 }
